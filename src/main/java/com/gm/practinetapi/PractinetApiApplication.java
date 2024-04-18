578d6207ce1071a9b04615664889d6e1c12b8fd5
wguia03 <126293468+wguia03@users.noreply.github.com>
2024-04-17 20:53:09 -0500

    initial commit

---
+++ src/main/java/com/gm/practinetapi/PractinetApiApplication.java
@@ -0,0 +1,13 @@
+package com.gm.practinetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractinetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractinetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gm/practinetapi/PractinetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.practinetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractinetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
